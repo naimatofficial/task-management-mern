921f20cc5667b0bc650854627377f88c5a264f06
naimatofficial <naimat.official.10@gmail.com>
2024-11-02 00:08:22 +0500

    update header profile

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -39,7 +39,7 @@ const Header = ({ profileMenuItems, toggleSidebar }) => {
 			</h1>
 			<div className="logo" />
 			<Dropdown
-				menu={<Menu items={profileMenuItems} />}
+				menu={{ items: profileMenuItems }}
 				trigger={["click"]}
 				placement="bottomRight"
 			>
